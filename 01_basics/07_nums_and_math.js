649add8e80e78db895d491de62ba191cc6279f13
HemanthS12 <hemanths8181@gmail.com>
2023-10-24 20:01:39 +0530

    math and num

--- 01_basics/07_nums_and_math.js
+++ 01_basics/07_nums_and_math.js
@@ -1,7 +1,32 @@
-const bal = 78000
-console.log(bal);
+// const bal = 78000
+// console.log(bal);
 
-const balance = new Number(80000)
-console.log(balance);
+// const balance = new Number(80000.8799)
+// console.log(balance);
 
-console.log("Hi");
+// console.log(balance.toString().length);
+// console.log(typeof (balance));
+// console.log(balance.toFixed(2));
+
+// const num1 = 178.9876
+// console.log(num1.toPrecision(3));
+
+// const num2 = new Number(10000000)
+// console.log(num2.toLocaleString('en-IN'));
+
+// ******************Maths****************
+
+// console.log(Math.abs(-600));
+// console.log(Math.round(87.61));
+// console.log(Math.ceil(87.61));
+// console.log(Math.floor(87.61));
+// console.log(Math.min(8, 4, 7, 1));
+// console.log(Math.max(8, 4, 7, 1));
+
+// Important
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+// const min = 1
+// const max = 6
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
