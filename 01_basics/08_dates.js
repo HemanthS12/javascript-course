d30d7fbc3ef7a14ca9bc085b0a32fb91e0a53ca3
HemanthS12 <hemanths8181@gmail.com>
2023-11-02 10:12:51 +0530

    date and time

--- 01_basics/08_dates.js
+++ 01_basics/08_dates.js
@@ -5,11 +5,31 @@
 // console.log(myDate.toLocaleString());
 // console.log(typeof myDate);
 
-let myCreatedDate = new Date(2023, 10, 1) // month starts from 0
-console.log("myCreatedDate is " + myCreatedDate.toDateString());
+// let myCreatedDate = new Date(2023, 10, 1) // month starts from 0
+// console.log("myCreatedDate is " + myCreatedDate.toDateString());
 
-let myCreatedDate1 = new Date(2023, 10, 1, 10, 43, 22) // month starts from 0
-console.log("myCreatedDate1 is " + myCreatedDate1.toLocaleString());
+// let myCreatedDate1 = new Date(2023, 10, 1, 10, 43, 22) // month starts from 0
+// console.log("myCreatedDate1 is " + myCreatedDate1.toLocaleString());
 
-let myCreatedDate2 = new Date("10-01-2023")
-console.log("myCreatedDate2 is " + myCreatedDate2.toLocaleString());
+// let myCreatedDate2 = new Date("10-01-2023")
+// console.log("myCreatedDate2 is " + myCreatedDate2.toLocaleString());
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate2.getTime());
+// //If we want to convert this in seconds
+// console.log(Math.floor(Date.now() / 1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getDate());
+// console.log(newDate.getMonth() + 1); //Index starts from 0
+// console.log(newDate.getDay()); //Mon-1, Tues-2
+// console.log(newDate.getFullYear());
+
+console.log(newDate.toLocaleString('default', {
+    month: "numeric",
+    day: "numeric",
+    weekday: "long",
+    year: "numeric"
+}));
