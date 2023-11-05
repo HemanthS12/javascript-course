413654c21ac71399f56e386ab6d64485884e9a11
HemanthS12 <hemanths8181@gmail.com>
2023-11-05 19:37:57 +0530

    Objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,7 +2,7 @@
 // When declared form constructor it would be singleton object
 
 //Singleton
-Object.create
+// Object.create
 
 //Object literals
 //Declaring objects:
@@ -23,19 +23,34 @@ const JsUser = {
     lastLoginDays: ["Monday", "Wednesday", "Friday"]
 }
 
-// To Access 
-console.log(JsUser.email); // One way
-console.log(JsUser["email"]); // 2nd way
-console.log(JsUser.state);
-console.log(JsUser.fullname);
-console.log(JsUser["fullname"]);
+// // To Access 
+// console.log(JsUser.email); // One way
+// console.log(JsUser["email"]); // 2nd way
+// console.log(JsUser.state);
+// console.log(JsUser.fullname);
+// console.log(JsUser["fullname"]);
 // console.log(typeof JsUser.mySym); // typeof gives string but we wanted it to be a symbol
-console.log(JsUser[mySym]);
-// console.log(typeof JsUser[mySym]);
+// console.log(JsUser[mySym]);
+// // console.log(typeof JsUser[mySym]);
 
-//To override data
-JsUser.email = "hemanth@google.com"
+// //To override data
+// JsUser.email = "hemanth@google.com"
+// // console.log(JsUser);
+// Object.freeze(JsUser) // freeze the object and later changes are not impacted
+// JsUser.email = "hemanth@cog.com"
 // console.log(JsUser);
-Object.freeze(JsUser) // freeze the object and later changes are not impacted
-JsUser.email = "hemanth@cog.com"
-console.log(JsUser);
+
+JsUser.greeting = function () {
+    console.log("Hello Js User");
+}
+console.log(JsUser.greeting());
+
+JsUser.greetingTwo = function () {
+    console.log(`Hello Js User, ${this.name}`); // When we have to reference same object then use 'this'
+}
+console.log(JsUser.greetingTwo());
+
+JsUser.ageOne = function () {
+    console.log(`${this.name} age is, ${this.age}`);
+}
+console.log(JsUser.ageOne());
