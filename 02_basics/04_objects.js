3d5e94c3f2722827c36f3e9ca76575241d3fbb6e
HemanthS12 <hemanths8181@gmail.com>
2023-11-07 18:06:57 +0530

    Objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,8 @@
+// const tinderUser = new Object() // Singleton object
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sam"
+tinderUser.isLoggedIn = "false"
+console.log(tinderUser);
