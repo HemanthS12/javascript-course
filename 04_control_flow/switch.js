cf17a13d018d878e313c791b990a1f9310bf53aa
HemanthS12 <hemanths8181@gmail.com>
2023-11-18 14:58:46 +0530

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,18 @@
+const month = "apr"
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("febrauary");
+        break;
+    case "mar":
+        console.log("march");
+        break;
+    case "apr":
+        console.log("april");
+        break;
+
+    default: console.log("default case match");
+        break;
+}
